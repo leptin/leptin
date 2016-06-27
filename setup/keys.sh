3920e6386321f2881e291e03c73f26bb4e55020e
Wolfgang <wolf@okwolf.com>
2016-06-26 20:44:44 -0700

    auto generated cert valid effectively forever for testing browsers seem to accept

--- setup/keys.sh
+++ setup/keys.sh
@@ -1,13 +1,53 @@
 #!/bin/sh -e
 # set -x
 
-mkdir -p /keys
-openssl dhparam -out /keys/dhparam.pem 2048
+CAROOT=/keys
+mkdir -p ${CAROOT}/ca.db.certs   # Signed certificates storage
+touch ${CAROOT}/ca.db.index      # Index of signed certificates
+echo 01 > ${CAROOT}/ca.db.serial # Next (sequential) serial number
+
+# Configuration
+cat>${CAROOT}/ca.conf<<'EOF'
+[ ca ]
+default_ca = ca_default
+
+[ ca_default ]
+dir = REPLACE_LATER
+certs = $dir
+new_certs_dir = $dir/ca.db.certs
+database = $dir/ca.db.index
+serial = $dir/ca.db.serial
+RANDFILE = $dir/ca.db.rand
+certificate = $dir/ca.crt
+private_key = $dir/ca.key
+default_days = 365
+default_crl_days = 30
+default_md = md5
+preserve = no
+policy = generic_policy
+[ generic_policy ]
+countryName = optional
+stateOrProvinceName = optional
+localityName = optional
+organizationName = optional
+organizationalUnitName = optional
+commonName = supplied
+emailAddress = optional
+EOF
+
+sed -i "s|REPLACE_LATER|${CAROOT}|" ${CAROOT}/ca.conf
+
+cd ${CAROOT}
+
+openssl dhparam -out dhparam.pem 2048
 
 # TODO: you should get these securely instead of generating
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-  -subj "/C=US/ST=Denial/L=Anytown/O=Evil Corp/CN=www.example.com" \
-  -keyout /keys/server.key -out /keys/server.crt
+openssl genrsa -out server.key 2048
+openssl req -new -key server.key -out server.csr \
+  -subj "/C=US/ST=Denial/L=Anytown/O=Evil Corp/CN=www.example.com" 
+openssl ca -batch -config ${CAROOT}/ca.conf -selfsign -keyfile server.key \
+  -in server.csr -out server.crt -md sha512 \
+  -startdate 00000101000000Z -enddate 99991231235959Z
 
 SSH_KEY_PATH=~/.ssh/id_rsa
 [ -f "$SSH_KEY_PATH" ] || ssh-keygen -t rsa -N "" -f $SSH_KEY_PATH
